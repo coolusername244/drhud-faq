0fb3e0d54a0a532b50deda4d96e293e4cda7f4ac
Lee <leesheppard2404@gmail.com>
2024-01-09 16:37:42 +0100

    feat: added smooth 'show' function for FAQs and only show one at a time

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,159 @@
-import Image from 'next/image'
+'use client';
+import { useState } from 'react';
+
+type Faq = {
+  question: string;
+  answer: JSX.Element;
+};
+
+type FaqsArray = Faq[];
+
+const faqs: FaqsArray = [
+  {
+    question: 'Varför finns Dr Hud?',
+    answer: (
+      <>
+        <p>
+          7 av 10 svenskar går idag runt med ett obehandlat hudproblem, och idag
+          behöver man vänta upp till 6 månader för att få kvalificerad hjälp,
+          trots remiss.
+        </p>
+        <p>
+          Vi startade Dr Hud för att vi anser att hudpatienter förtjänar en
+          bättre vårdupplevelse. Flera av oss har själva drabbats av hudproblem,
+          långa väntetider och felaktiga bedömningar, och vi är tyvärr inte
+          ensamma.
+        </p>
+        <p>
+          Teamet, som består av både hudläkare och personer med mångårig
+          erfarenhet från vård och digital hälsa, har som mål att erbjuda en
+          tjänst som är bättre för såväl patienter, läkare och vårdsystemet i
+          sin helhet.
+        </p>
+      </>
+    ),
+  },
+  {
+    question: 'Hur kvalificerade är era läkare?',
+    answer: (
+      <p>
+        Hudläkarna som hjälper dig via Dr Hud tjänst är utbildade dermatologer
+        med svensk läkarlegitimation. Samtliga har mångårig erfarenhet från
+        privata och/eller publika specialistmottagningar.
+      </p>
+    ),
+  },
+  {
+    question: 'Kan jag använda Dr Hud för alla hudåkommor?',
+    answer: (
+      <>
+        <p>
+          Våra hudläkare kan bedöma {'>'} 3000 olika hudbesvär digitalt. Även de
+          problem som kräver fysiskt besök (biopsi) och behandling kommer inom
+          kort att kunna remitteras via tjänsten.
+        </p>
+        <p>
+          Värt att veta är att tjänsten även kan användas för problem såsom
+          insektsbett, hår-/håravfall, naglar, svettningar och sår.
+        </p>
+      </>
+    ),
+  },
+  {
+    question: 'Varför behöver jag ladda upp bilder?',
+    answer: (
+      <p>
+        Bilder utgör tillsammans med dina svar i frågeformulären underlag för
+        din bedömning. Skulle bilderna vara bristfälliga kommer läkaren be dig
+        ta nya sådana.
+      </p>
+    ),
+  },
+  {
+    question: 'Kan jag ställa frågor till läkaren?',
+    answer: (
+      <p>
+        Självklart. Frågor ställer du enklast i samband med att du skickar in
+        ditt ärende. Skulle något fortfarande vara oklart efter att du fått ditt
+        svar har du en vecka på dig att svara, via email eller direkt via
+        plattformen.
+      </p>
+    ),
+  },
+  {
+    question: 'Vad händer om jag behöver recept?',
+    answer: (
+      <p>
+        Recept erbjuds på Dr Hud sedan november 2023. Samtliga ärenden som
+        bedöms via Dr Hud kan tillskrivas recept, givet att hudläkaren bedömer
+        att sådan ordination är lämplig för den åkomma du sökt för.
+      </p>
+    ),
+  },
+];
 
 export default function Home() {
+  const [isVisible, setIsVisible] = useState<number | null>(null);
+  const [maxHeight, setMaxHeight] = useState<number | null>(null);
+
+  const toggleAnswerVisibility = (index: number) => {
+    const newMaxHeight = isVisible === index ? null : index;
+    setMaxHeight(newMaxHeight);
+    setIsVisible(newMaxHeight);
+  };
+
+  const isArrowRotated = (index: number) => {
+    return isVisible === index ? 'rotate-180' : '';
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <section id="faq" className="my-20 mx-5 mt-40">
+      <div className="max-w-5xl mx-auto">
+        <h2 className="text-center text-[3.0rem] leading-[3.5rem] md:text-6xl mb-20 text-gray-800 font-semibold">
+          Vanliga frågor
+        </h2>
+        <div className="divide-y">
+          {faqs.map((faq, i) => {
+            return (
+              <div key={i}>
+                <div
+                  role="button"
+                  onClick={() => toggleAnswerVisibility(i)}
+                  className="my-5 flex justify-between text-gray-700 cursor-pointer"
+                >
+                  <div className="font-medium text-xl">{faq.question}</div>
+                  <div>
+                    <svg
+                      className={`transition-transform transform ${isArrowRotated(
+                        i,
+                      )}`}
+                      stroke="currentColor"
+                      fill="currentColor"
+                      strokeWidth="0"
+                      viewBox="0 0 16 16"
+                      height="1em"
+                      width="1em"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708l6-6z"
+                      ></path>
+                    </svg>
+                  </div>
+                </div>
+                <div
+                  className={`text-gray-700 overflow-hidden transition-all ${
+                    maxHeight === i ? 'max-h-96 duration-500' : 'max-h-0'
+                  }`}
+                >
+                  <div className="flex flex-col gap-4">{faq.answer}</div>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </section>
+  );
 }
